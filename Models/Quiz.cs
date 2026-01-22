4ca70d079dc1b9809eb61112dae8ba93466bffd3
SufriedJakub <sufriedjakub@icloud.com>
2026-01-22 13:56:30 +0100

    Quiz.cs

--- Models/Quiz.cs
+++ Models/Quiz.cs
@@ -1,18 +1,8 @@
 using System.Collections.Generic;
 
-public class Quiz : IQuiz
+public class Quiz : IQuiz<Pytanie, Odpowiedz>
 {
-    public string Nazwa { get; set; }
-    public List<IQuestion> Pytania { get; set; }
-
-    public Quiz(string nazwa)
-    {
-        Nazwa = nazwa;
-        Pytania = new List<IQuestion>();
-    }
-
-    public void DodajPytanie(IQuestion pytanie)
-    {
-        Pytania.Add(pytanie);
-    }
+    public int Id { get; set; }
+    public string Nazwa { get; set; } = "";
+    public List<Pytanie> Pytania { get; set; } = new();
 }
